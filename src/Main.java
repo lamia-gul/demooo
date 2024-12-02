144c2b2b26529ef42383da4366233ca5c411b7ca
lamia-gul <lamiagul56@gmail.com>
2024-12-02 17:22:04 +0500

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
-
+        System.out.println("hello All group members");
         System.out.println("lamia here");
         System.out.println("shaila here");
 
